910bfdea9d6130f52cbf017d17d3220dca53e886
WakeupTsai <c20712983@gmail.com>
2016-10-30 16:00:12 +0200

    Capture image every second.

---
+++ capture_and_server/capture.cpp
@@ -0,0 +1,50 @@
+#include "opencv2/highgui/highgui.hpp"
+#include <unistd.h>
+#include <ctime>
+#include <string>
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+using namespace cv;
+
+void detectAndDisplay( Mat frame );
+
+/** @function main */
+int main( void )
+{
+    VideoCapture capture;
+    Mat frame;
+
+    capture.open( -1 );
+    if ( ! capture.isOpened() ){
+        cout << "! capture.isOpened() " << endl;
+        return -1;
+    }
+    char filename[]="LastPicture.txt"; 
+    fstream fp;
+
+    while(1){
+	fp.open(filename, ios::out|ios::app);
+
+    	time_t     now = time(0);
+    	struct tm  tstruct;
+    	char       buf[80];
+    	tstruct = *localtime(&now);
+    	strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+
+    	string recordTime = buf;
+    	string filename = buf;
+	capture.read(frame);
+    	capture.read(frame);
+    	imwrite("picture/"+ filename +".png", frame);
+    
+	cout << "Time: "<<recordTime << endl;
+	fp << filename + ".png" << endl;
+	fp.close();
+	sleep(1);
+
+    }
+    return 0;
+}
+
